41d42e4fb79db4e2d1fc7211939883052f7bed52
Sameer Hasan <simplysamy@gmail.com>
2025-04-11 08:14:17 +0530

    Add new resource - github file

--- main.tf
+++ main.tf
@@ -7,3 +7,11 @@ resource "github_repository" "mtc_repo" {
   auto_init   = true
 
 }
+
+resource "github_repository_file" "readme" {
+  repository          = github_repository.mtc_repo.name
+  branch              = "main"
+  file                = "README.md"
+  content             = "# This repos is for infra developer"
+  overwrite_on_create = true
+}
